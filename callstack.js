00691eb34d80483a145b2f0501fb46d046100eef
htam00 <anonimozurik@gmail.com>
2019-07-05 08:13:11 -0300

    add file callstack.js

---
+++ callstack.js
@@ -0,0 +1,16 @@
+function sayHello(){
+  console.log('Hello my name is htam00')
+}
+
+function sayAge(){
+ console.log('my age is 20')
+}
+
+function sayAll(){
+ sayHello()
+ sayAge()
+}
+
+sayAll()
+
+console.log('final')
